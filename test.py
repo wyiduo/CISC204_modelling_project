841f20c8041f21c17e2090193e7f95ab8c4c0d74
Yiduo Wang <23614440+wyiduo@users.noreply.github.com>
2021-12-14 00:24:45 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 
 import os, sys
 
-from run import example_theory
+from run import optimal_team_comp
 
 USAGE = '\n\tpython3 test.py [draft|final]\n'
 EXPECTED_VAR_MIN = 10
 EXPECTED_CONS_MIN = 50
 
 def test_theory():
-    T = example_theory()
+    T = optimal_team_comp()
 
     assert len(T.vars()) > EXPECTED_VAR_MIN, "Only %d variables -- your theory is likely not sophisticated enough for the course project." % len(T.vars())
     assert T.size() > EXPECTED_CONS_MIN, "Only %d operators in the formula -- your theory is likely not sophisticated enough for the course project." % T.size()
